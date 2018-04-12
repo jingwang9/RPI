1e272776690e903340fad090826e8fd7f4f6d052
jingwang9 <35493495+jingwang9@users.noreply.github.com>
2018-04-12 02:46:44 +0100

    motor2

--- motor.cpp
+++ motor.cpp
@@ -1,51 +1,51 @@
+
+#include <stdio.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
-#include <stdio.h>
-#include <stdlib.h>
 
 static int pin_A, pin_B, pin_E;
-void set_L293D(int);
-void init_L293D()
-{
-    pin_A = 16;
-    pin_B = 18;
-    pin_E = 22;
-    pinMode(pin_E, OUTPUT);
-    pinMode(pin_A, OUTPUT);
-    pinMode(pin_B, OUTPUT);
-    set_L293D(0);
-}
 
 void set_L293D(int value)
 {
-    if(value == 0) {
-        digitalWrite(pin_E, LOW);
-    } else if(value > 0 && value <= 255) {
-        digitalWrite(pin_E, LOW);
-        digitalWrite(pin_A, HIGH);
-        digitalWrite(pin_B, LOW);
-        analogWrite(pin_E, value);
-    } else if(value < 0 && value >= -255) {
-        digitalWrite(pin_E, LOW);
-        digitalWrite(pin_A, LOW);
-        digitalWrite(pin_B, HIGH);
-        analogWrite(pin_E, value);
-    }
+        if(value == 0) {
+                digitalWrite(pin_E, LOW);
+        } else if(value > 0 && value <= 255) {
+                digitalWrite(pin_E, LOW);
+                digitalWrite(pin_A, HIGH);
+                digitalWrite(pin_B, LOW);
+                analogWrite(pin_E, value);
+        } else if(value < 0 && value >= -255) {
+                digitalWrite(pin_E, LOW);
+                digitalWrite(pin_A, LOW);
+                digitalWrite(pin_B, HIGH);
+                analogWrite(pin_E, value);
+        }
+}
+
+void init_L293D(int pinA, int pinB, int pinE)
+{
+        pin_A = pinA;
+        pin_B = pinB;
+        pin_E = pinE;
+        pinMode(pin_E, OUTPUT);
+        pinMode(pin_A, OUTPUT);
+        pinMode(pin_B, OUTPUT);
+        wiringPisetup();
+        set(0);
 }
 
 int main(int argc, char**argv)
 {
-    int pin_A, pinB, pinE;
-    int value;
-    init_L293D();
-    while(1) {
-        printf("Input the value\n");
-        scanf("%d", &value);
-        if(value>255||value<-255) {
-            printf("Invalid value(Must -255~255)\n");
-            continue;
+        int value;
+        init_L293D(16,18,22);
+        while(1) {
+                printf("Input the value\n");
+                scanf("%d", &value);
+                if(value>255||value<-255) {
+                        printf("Invalid value(Must -255~255)\n");
+                        continue;
+                }
+                set_L293D(value);
         }
-        set_L293D(value);
-    }
-    return 0;
+        return 0;
 }
